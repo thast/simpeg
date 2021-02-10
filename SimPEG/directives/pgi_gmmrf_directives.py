6ad62b3f6a604b830bbe0fd1201b487f5c9973c5
Thibaut Astic <thibaut.astic@gmail.com>
2021-02-10 15:02:10 -0800

    initial commit:revive old code

---
+++ SimPEG/directives/pgi_gmmrf_directives.py
@@ -0,0 +1,101 @@
+from __future__ import print_function
+
+import numpy as np
+import matplotlib.pyplot as plt
+import copy
+from ..regularization import (
+    SimpleSmall,
+    Small,
+    SparseSmall,
+    Simple,
+    Tikhonov,
+    Sparse,
+    SimplePGIsmallness,
+    PGIsmallness,
+    SimplePGIwithNonlinearRelationshipsSmallness,
+    SimplePGI,
+    PGI,
+    SmoothDeriv,
+    SimpleSmoothDeriv,
+    SparseDeriv,
+    SimplePGIwithRelationships,
+)
+from ..utils import (
+    mkvc,
+    WeightedGaussianMixture,
+    GaussianMixtureWithPrior,
+    GaussianMixtureWithNonlinearRelationships,
+    GaussianMixtureWithNonlinearRelationshipsWithPrior,
+    Zero,
+)
+from ..directives import InversionDirective, MultiTargetMisfits
+from ..utils.code_utils import deprecate_property
+
+class SmoothUpdateReferenceModel(InversionDirective):
+
+    neighbors = None
+    distance = 2
+    weigthed_random_walk = True
+    compute_score = False
+    maxit = None
+    verbose = False
+    method = 'ICM'  # 'Gibbs'
+    offdiag = 0.
+    indiag = 1.
+    Pottmatrix = None
+    log_univar = None
+
+    def endIter(self):
+        mesh = self.invProb.reg._mesh
+        if self.neighbors is None:
+            self.neighbors = 2 * mesh.dim
+
+        m = self.invProb.model
+        modellist = self.invProb.reg.wiresmap * m
+        model = np.c_[
+            [a * b for a, b in zip(self.invProb.reg.maplist, modellist)]].T
+        minit = self.invProb.reg.GMmodel.predict(model)
+
+        indActive = self.invProb.reg.indActive
+
+        if self.Pottmatrix is None:
+            n_unit = self.invProb.reg.GMmodel.n_components
+            Pott = np.ones([n_unit, n_unit]) * self.offdiag
+            for i in range(Pott.shape[0]):
+                Pott[i, i] = self.indiag
+            self.Pottmatrix = Pott
+
+        # if self.log_univar is None:
+        _, self.log_univar = self.invProb.reg.GMmodel._estimate_log_prob_resp(
+            model
+        )
+
+        if self.method == 'Gibbs':
+            denoised = Utils.GibbsSampling_PottsDenoising(
+                mesh, minit,
+                self.log_univar,
+                self.Pottmatrix,
+                indActive=indActive,
+                neighbors=self.neighbors,
+                norm=self.distance,
+                weighted_selection=self.weigthed_random_walk,
+                compute_score=self.compute_score,
+                maxit=self.maxit,
+                verbose=self.verbose
+            )
+        elif self.method == 'ICM':
+            denoised = Utils.ICM_PottsDenoising(
+                mesh, minit,
+                self.log_univar,
+                self.Pottmatrix,
+                indActive=indActive,
+                neighbors=self.neighbors,
+                norm=self.distance,
+                weighted_selection=self.weigthed_random_walk,
+                compute_score=self.compute_score,
+                maxit=self.maxit,
+                verbose=self.verbose
+            )
+
+        self.invProb.reg.mref = Utils.mkvc(
+            self.invProb.reg.GMmodel.means_[denoised[0]])
